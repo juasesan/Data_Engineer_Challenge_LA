70d483e37622efb6ac123294603d7a38b1ec6864
juasesan <jssa132@gmail.com>
2024-03-23 23:36:25 -0500

    Creates q1 function for memory and time optimization

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,22 @@
 from typing import List, Tuple
 from datetime import datetime
+import pandas as pd
+import json
+
+from functions import first_mode
+
 
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    with open(file_path, "r") as tweets_file:
+        df = [(json.loads(line)['date'], json.loads(line)['user']['username']) for line in tweets_file]
+    
+    df = pd.DataFrame(df, columns=['date', 'username'])
+    df['date'] = pd.to_datetime(df['date']).dt.date
+
+    df = df.groupby('date').agg({'username':[lambda x: x.count(), first_mode]}).reset_index()
+    df.columns=['date', 'count', 'user']
+
+    df = df.sort_values(by='count', ascending=False).head(10)
+
+    return list(zip(df['date'], df['user']))
+
--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,22 @@
 from typing import List, Tuple
 from datetime import datetime
+import pandas as pd
+import json
+
+from functions import first_mode
+
 
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    with open(file_path, "r") as tweets_file:
+        df = [(json.loads(line)['date'], json.loads(line)['user']['username']) for line in tweets_file]
+
+    df = pd.DataFrame(df, columns=['date', 'username'])
+    df['date'] = pd.to_datetime(df['date']).dt.date
+
+    top_dates = df['date'].value_counts().head(10).index.tolist()
+
+    df = df[df['date'].isin(top_dates)]
+
+    df = df.groupby('date').agg({'username': first_mode}).reset_index()
+
+    return list(zip(df['date'], df['username']))
