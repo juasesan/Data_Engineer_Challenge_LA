89e4452b5eb4a600ae88c5956ec928e6f02e1c80
juasesan <jssa132@gmail.com>
2024-03-23 23:46:10 -0500

    Agrega comentarios a las funciones creadas

--- src/functions.py
+++ src/functions.py
@@ -1,2 +1,9 @@
 def first_mode(series):
+    """"
+    Obtiene el valor más común dentro de una serie (moda)
+    Args:
+        series: Serie de valores
+    Returns:
+        El valor de moda
+    """
     return series.mode().iloc[0]
--- src/q1_time.py
+++ src/q1_time.py
@@ -7,16 +7,19 @@ from functions import first_mode
 
 
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
+    # Lectura del json y extracción de los campos de fecha y usuario
     with open(file_path, "r") as tweets_file:
         df = [(json.loads(line)['date'], json.loads(line)['user']['username']) for line in tweets_file]
 
+    # Conversión a dataframe con el formato de fecha adecuado
     df = pd.DataFrame(df, columns=['date', 'username'])
     df['date'] = pd.to_datetime(df['date']).dt.date
 
+    # Obtención de las 10 fechas con más tweets y filtrado de estas en el dataframe
     top_dates = df['date'].value_counts().head(10).index.tolist()
-
     df = df[df['date'].isin(top_dates)]
 
+    # Obtención del usuario más repetido en cada fecha
     df = df.groupby('date').agg({'username': first_mode}).reset_index()
 
     return list(zip(df['date'], df['username']))
