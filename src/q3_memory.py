03b8fad787cee434b4aac660a616a11ebf05bdd6
juasesan <jssa132@gmail.com>
2024-03-24 20:14:36 -0500

    Creates q3 functions

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,4 +1,16 @@
 from typing import List, Tuple
+from collections import Counter
+import json
+
 
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    def generate_usernames(file):
+        for line in file:
+            users_mentioned = json.loads(line).get('mentionedUsers')
+            if users_mentioned:
+                for user in users_mentioned:
+                    yield user['username']
+    
+    users_count = Counter(generate_usernames(open(file_path, "r")))
+    
+    return users_count.most_common(10)
--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,15 @@
 from typing import List, Tuple
+from collections import Counter
+import json
+
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    users_count = Counter()
+
+    with open(file_path, "r") as tweets_file:
+        for line in tweets_file:
+            users_mentioned = json.loads(line)['mentionedUsers']
+            if users_mentioned:
+                users_count.update([user['username'] for user in users_mentioned])
+
+    return users_count.most_common(10)
