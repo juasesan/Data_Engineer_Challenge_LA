ec29e3ec85e7ed384a918c0a2f0116e257ee117a
juasesan <jssa132@gmail.com>
2024-03-24 19:29:36 -0500

    Agrega comentarios a las funciones q2

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -5,11 +5,14 @@ import json
 
 
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
+    # Inicialización del contador de emojis
     emoji_counts = Counter()
 
     with open(file_path, "r") as tweets_file:
         for line in tweets_file:
+            # Por cada tweet, se obtienen los emojis presentes en una lista y se actualiza el contador
             emojis = extract_emojis(json.loads(line)['content'])
             emoji_counts.update(emojis)
     
+    # Retorno de los 10 emojis más comunes
     return emoji_counts.most_common(10)
--- src/q2_time.py
+++ src/q2_time.py
@@ -5,13 +5,17 @@ import json
 
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
+    # Lectura de los tweets en el archivo json
     with open(file_path, "r") as tweets_file:
         tweets = [json.loads(line)['content'] for line in tweets_file]
     
+    # Inicialización del contador
     emoji_counts = Counter()
 
+    # Por cada tweet, se extraen los emojis en una lista y se actualiza el contador
     for tweet in tweets:
         emojis = extract_emojis(tweet)
         emoji_counts.update(emojis)
     
+    # Retorno de los 10 emojis más comunes
     return emoji_counts.most_common(10)
