51beb8c8b571fae242f6bd8edb6e9e3ab56b032a
juasesan <jssa132@gmail.com>
2024-03-24 19:24:35 -0500

    Creates q2 functions

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,15 @@
 from typing import List, Tuple
+from functions import extract_emojis
+from collections import Counter
+import json
+
 
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    emoji_counts = Counter()
+
+    with open(file_path, "r") as tweets_file:
+        for line in tweets_file:
+            emojis = extract_emojis(json.loads(line)['content'])
+            emoji_counts.update(emojis)
+    
+    return emoji_counts.most_common(10)
--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,17 @@
 from typing import List, Tuple
+from functions import extract_emojis
+from collections import Counter
+import json
+
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    with open(file_path, "r") as tweets_file:
+        tweets = [json.loads(line)['content'] for line in tweets_file]
+    
+    emoji_counts = Counter()
+
+    for tweet in tweets:
+        emojis = extract_emojis(tweet)
+        emoji_counts.update(emojis)
+    
+    return emoji_counts.most_common(10)
