2275f11d92aab2e4b47198a71c4d96d4f8fc08de
juasesan <jssa132@gmail.com>
2024-03-24 20:56:46 -0500

    Adds comments in q3 functions

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -4,6 +4,7 @@ import json
 
 
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
+    # Definición un generador que retorne los usuarios listados en la sección de mentionedUsers.
     def generate_usernames(file):
         for line in file:
             users_mentioned = json.loads(line).get('mentionedUsers')
@@ -11,6 +12,8 @@ def q3_memory(file_path: str) -> List[Tuple[str, int]]:
                 for user in users_mentioned:
                     yield user['username']
     
+    # Uso de la "lazy evaluation" al contar los usuarios utilizando un generador en vez de obtener todos y guardarlos en memoria a la vez.
     users_count = Counter(generate_usernames(open(file_path, "r")))
     
+    # Retorno de los 10 usuarios más mencionados.
     return users_count.most_common(10)
--- src/q3_time.py
+++ src/q3_time.py
@@ -4,13 +4,16 @@ import json
 
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
+    # Inicialización del contador de usuarios
     users_count = Counter()
 
     with open(file_path, "r") as tweets_file:
         for line in tweets_file:
+            # Conteo de usuarios mencionados en caso de existir (mentionedUsers no es None), caso contrario, pasar por alto.
             try:
                 users_count.update([user['username'] for user in json.loads(line)['mentionedUsers']])
             except:
                 pass
 
+    # Retorno de los 10 usuarios más mencionados.
     return users_count.most_common(10)
